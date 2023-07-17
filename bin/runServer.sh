18809c3501750794ab449616dcf265f35e90c9f4
haiyang <491680922@qq.com>
2023-07-17 11:24:36 +0800

    排除 tools.jar 打包

--- bin/runServer.sh
+++ bin/runServer.sh
@@ -1,3 +1,4 @@
 
 CURRENT_DIR=$(cd `dirname $0`; pwd)
-java -jar macaque-server-1.0.jar --agentpath=$CURRENT_DIR/macaque-agent.jar --debug
+JAVA_HOME=''
+$JAVA_HOME/bin/java -Djava.ext.dirs=$JAVA_HOME/lib -jar macaque-server-1.0.jar --agentpath=$CURRENT_DIR/macaque-agent.jar --debug
