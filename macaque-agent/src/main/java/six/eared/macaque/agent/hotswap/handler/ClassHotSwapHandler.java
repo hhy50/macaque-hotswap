24f032f877ad5851e00fe28b371faddb08c3d3fc
haiyang <491680922@qq.com>
2023-09-26 09:24:20 +0800

    代码优化

--- macaque-agent/src/main/java/six/eared/macaque/agent/hotswap/handler/ClassHotSwapHandler.java
+++ macaque-agent/src/main/java/six/eared/macaque/agent/hotswap/handler/ClassHotSwapHandler.java
@@ -1,10 +1,14 @@
 package six.eared.macaque.agent.hotswap.handler;
 
 import six.eared.macaque.agent.annotation.HotSwapFileType;
+import six.eared.macaque.agent.asm2.AsmUtil;
 import six.eared.macaque.agent.asm2.classes.ClazzDefinition;
+import six.eared.macaque.agent.asm2.classes.ClazzDefinitionVisitorFactory;
+import six.eared.macaque.agent.asm2.enhance.CompatibilityModeByteCodeEnhancer;
 import six.eared.macaque.agent.env.Environment;
 import six.eared.macaque.agent.vcs.VersionChainTool;
 import six.eared.macaque.agent.vcs.VersionView;
+import six.eared.macaque.common.ExtPropertyName;
 import six.eared.macaque.common.type.FileType;
 import six.eared.macaque.mbean.rmi.HotSwapRmiData;
 import six.eared.macaque.mbean.rmi.RmiResult;
@@ -12,7 +16,6 @@ import six.eared.macaque.mbean.rmi.RmiResult;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static six.eared.macaque.agent.hotswap.ClassHotSwapper.redefine;
 
@@ -26,17 +29,23 @@ public class ClassHotSwapHandler extends FileHookHandler {
         return handler(bytes, extProperties);
     }
 
+    @SuppressWarnings("unchecked")
     public RmiResult handler(byte[] bytes, Map<String, String> extProperties) {
         String errMsg = null;
         try {
             Map<String, Object> result = new HashMap<>();
-
             VersionView versionView = VersionChainTool.getActiveVersionView();
-            List<ClazzDefinition> definitions = versionView.getDefinitions().stream()
-                    .map(ClazzDefinition.class::cast)
-                    .collect(Collectors.toList());
+            boolean compatibilityMode = Boolean.TRUE.toString().equalsIgnoreCase(extProperties.get(ExtPropertyName.COMPATIBILITY_MODE));
 
+            List<ClazzDefinition> definitions = AsmUtil.readMultiClass(bytes, compatibilityMode
+                    ? ClazzDefinitionVisitorFactory.COMPATIBILITY_MODE
+                    : ClazzDefinitionVisitorFactory.DEFAULT);
+            versionView.setDefinitions((List) definitions);
             for (ClazzDefinition definition : definitions) {
+                if (compatibilityMode) {
+                    byte[] enhance = CompatibilityModeByteCodeEnhancer.enhance(definition.getByteCode());
+                    definition.setByteCode(enhance);
+                }
                 int redefineCount = redefine(definition);
                 result.put(definition.getClassName(), redefineCount);
             }
--- macaque-agent/src/main/java/six/eared/macaque/agent/vcs/VersionChainRecorder.java
+++ macaque-agent/src/main/java/six/eared/macaque/agent/vcs/VersionChainRecorder.java
@@ -1,40 +1,18 @@
 package six.eared.macaque.agent.vcs;
 
-import six.eared.macaque.agent.asm2.AsmUtil;
-import six.eared.macaque.agent.asm2.classes.ClazzDefinition;
-import six.eared.macaque.agent.asm2.classes.ClazzDefinitionVisitorFactory;
-import six.eared.macaque.agent.asm2.enhance.CompatibilityModeByteCodeEnhancer;
 import six.eared.macaque.agent.definition.FileDefinition;
-import six.eared.macaque.common.ExtPropertyName;
 import six.eared.macaque.common.type.FileType;
 import six.eared.macaque.library.hook.HotswapHook;
 import six.eared.macaque.mbean.rmi.HotSwapRmiData;
 import six.eared.macaque.mbean.rmi.RmiResult;
 
-import java.util.List;
-import java.util.Map;
-
 
 public class VersionChainRecorder implements HotswapHook {
 
     @Override
-    @SuppressWarnings("unchecked")
     public RmiResult executeBefore(HotSwapRmiData rmiData) {
         VersionView versionView = VersionChainTool.startNewEpoch();
-        Map<String, String> extProperties = rmiData.getExtProperties();
-        if (FileType.Class.match(rmiData.getFileType())) {
-            boolean compatibilityMode = Boolean.TRUE.toString().equalsIgnoreCase(extProperties.get(ExtPropertyName.COMPATIBILITY_MODE));
-            List<ClazzDefinition> definitions = AsmUtil.readMultiClass(rmiData.getFileData(),
-                    compatibilityMode ? ClazzDefinitionVisitorFactory.COMPATIBILITY_MODE
-                            : ClazzDefinitionVisitorFactory.DEFAULT);
-            versionView.setDefinitions((List) definitions);
-            if (compatibilityMode) {
-                for (ClazzDefinition definition : definitions) {
-                    byte[] enhance = CompatibilityModeByteCodeEnhancer.enhance(definition.getByteCode());
-                    definition.setByteCode(enhance);
-                }
-            }
-        } else {
+        if (!FileType.Class.match(rmiData.getFileType())) {
             FileDefinition fileDefinition = new FileDefinition();
             fileDefinition.setName(rmiData.getFileName());
             fileDefinition.setFileType(rmiData.getFileType());
