5100683554b649ab9515cf64a1d53af85b4cc8ef
haiyang <491680922@qq.com>
2023-10-18 16:06:09 +0800

    补充提交

--- macaque-agent/src/main/java/six/eared/macaque/agent/hotswap/handler/ClassHotSwapHandler.java
+++ macaque-agent/src/main/java/six/eared/macaque/agent/hotswap/handler/ClassHotSwapHandler.java
@@ -4,6 +4,7 @@ import six.eared.macaque.agent.annotation.HotSwapFileType;
 import six.eared.macaque.agent.asm2.AsmUtil;
 import six.eared.macaque.agent.asm2.classes.ClazzDefinition;
 import six.eared.macaque.agent.asm2.classes.ClazzDefinitionVisitorFactory;
+import six.eared.macaque.agent.asm2.classes.CorrelationClazzDefinition;
 import six.eared.macaque.agent.asm2.enhance.CompatibilityModeByteCodeEnhancer;
 import six.eared.macaque.agent.hotswap.ClassHotSwapper;
 import six.eared.macaque.agent.vcs.VersionChainTool;
@@ -13,8 +14,10 @@ import six.eared.macaque.common.type.FileType;
 import six.eared.macaque.mbean.rmi.HotSwapRmiData;
 import six.eared.macaque.mbean.rmi.RmiResult;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @HotSwapFileType(fileType = FileType.Class)
@@ -33,21 +36,24 @@ public class ClassHotSwapHandler extends FileHookHandler {
         boolean compatibilityMode = Boolean.TRUE.toString()
                 .equalsIgnoreCase(extProperties.get(ExtPropertyName.COMPATIBILITY_MODE));
 
-        if (compatibilityMode) {
-            for (ClazzDefinition definition : definitions) {
+        for (ClazzDefinition definition : definitions) {
+            if (compatibilityMode) {
                 CompatibilityModeByteCodeEnhancer
                         .enhance(definition);
-                versionView.addDefinition(definition);
             }
+            versionView.addDefinition(definition);
         }
-
         return RmiResult.success().data(ClassHotSwapper
                 .redefine(flatClassDefinition(definitions)));
     }
 
-    private ClazzDefinition flatClassDefinition(List<ClazzDefinition> definitions) {
-
-
-        return null;
+    private List<ClazzDefinition> flatClassDefinition(List<ClazzDefinition> definitions) {
+        List<ClazzDefinition> result = new ArrayList<>();
+        for (ClazzDefinition definition : definitions) {
+            result.add(definition);
+            result.addAll(definition.getCorrelationClasses().stream().map(CorrelationClazzDefinition::getClazzDefinition)
+                    .collect(Collectors.toList()));
+        }
+        return result;
     }
 }
