f51f9b155f5c0f436b38491e6785c08fd3b78cbd
haiyang <491680922@qq.com>
2023-11-11 23:30:47 +0800

    补充提交

--- macaque-agent/src/main/java/six/eared/macaque/agent/AgentBootstrap.java
+++ macaque-agent/src/main/java/six/eared/macaque/agent/AgentBootstrap.java
@@ -12,11 +12,39 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.rmi.registry.LocateRegistry;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AgentBootstrap {
+
+    static {
+        try {
+            String javaHome = System.getProperty("java.home");
+            String toolsJarURL = "file:" + javaHome + "/../lib/tools.jar";
+
+            // Make addURL public
+            Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            method.setAccessible(true);
+
+            URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+            if (sysloader.getResourceAsStream("/com/sun/tools/attach/VirtualMachine.class") == null) {
+                method.invoke(sysloader, (Object) new URL(toolsJarURL));
+                Thread.currentThread().getContextClassLoader().loadClass("com.sun.tools.attach.VirtualMachine");
+                Thread.currentThread().getContextClassLoader().loadClass("com.sun.tools.attach.AttachNotSupportedException");
+            }
+
+        } catch (Exception e) {
+            if (Environment.isDebug()) {
+                System.out.println("Java home points to " + System.getProperty("java.home") + " make sure it is not a JRE path");
+                System.out.print("Failed to add tools.jar to classpath: ");
+                e.printStackTrace();
+            }
+        }
+    }
 
     private static final AtomicBoolean START_FLAG = new AtomicBoolean(false);
 
