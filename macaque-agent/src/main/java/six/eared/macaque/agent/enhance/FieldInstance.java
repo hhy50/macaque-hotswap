ae3411fb414dd728ac4fa2518484dacf0dd29ed3
haiyang <491680922@qq.com>
2024-07-19 13:03:49 +0800

    测试

---
+++ macaque-agent/src/main/java/six/eared/macaque/agent/enhance/FieldInstance.java
@@ -0,0 +1,23 @@
+package six.eared.macaque.agent.enhance;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import six.eared.macaque.agent.asm2.AsmField;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FieldInstance {
+
+    private AsmField asmField;
+
+    public String getFieldName() {
+        return asmField.getFieldName();
+    }
+
+    public String getDesc() {
+        return asmField.getDesc();
+    }
+}
