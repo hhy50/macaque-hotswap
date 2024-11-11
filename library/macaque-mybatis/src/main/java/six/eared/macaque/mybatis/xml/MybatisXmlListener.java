34d83fd8c989ed26a6cfa89c75a9dc1b6c641ef9
haiyang <491680922@qq.com>
2024-11-11 23:59:23 +0800

    mybatis todo

--- library/macaque-mybatis/src/main/java/six/eared/macaque/mybatis/xml/MybatisXmlListener.java
+++ library/macaque-mybatis/src/main/java/six/eared/macaque/mybatis/xml/MybatisXmlListener.java
@@ -1,21 +1,40 @@
 package six.eared.macaque.mybatis.xml;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 import six.eared.macaque.library.hook.HotswapHook;
 import six.eared.macaque.mbean.rmi.HotSwapRmiData;
 import six.eared.macaque.mbean.rmi.RmiResult;
+
+import javax.xml.bind.annotation.XmlElement;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MybatisXmlListener implements HotswapHook {
 
     @Override
     public RmiResult executeBefore(HotSwapRmiData rmiData) {
-        System.out.println("executeBefore...");
-        return null;
+        byte[] xmlData = rmiData.getFileData();
+        try (ByteArrayInputStream in = new ByteArrayInputStream(xmlData)) {
+            XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+            xmlReader.parse(new InputSource(in));
+            String namespace = (String) xmlReader.getProperty("namespace");
+
+        } catch (IOException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return RmiResult.success();
     }
 
     @Override
     public RmiResult executeAfter(HotSwapRmiData rmiData, RmiResult result, Throwable error) {
-        System.out.println("executeAfter...");
         return null;
     }
 }
