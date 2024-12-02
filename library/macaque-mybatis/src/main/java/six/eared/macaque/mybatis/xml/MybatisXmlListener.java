45385b78150608955bcff71b947ce3c581852d82
haiyang <491680922@qq.com>
2024-12-03 00:19:43 +0800

    1

--- library/macaque-mybatis/src/main/java/six/eared/macaque/mybatis/xml/MybatisXmlListener.java
+++ library/macaque-mybatis/src/main/java/six/eared/macaque/mybatis/xml/MybatisXmlListener.java
@@ -1,13 +1,14 @@
 package six.eared.macaque.mybatis.xml;
 
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
+
 import six.eared.macaque.library.hook.HotswapHook;
 import six.eared.macaque.mbean.rmi.HotSwapRmiData;
 import six.eared.macaque.mbean.rmi.RmiResult;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 import javax.xml.bind.annotation.XmlElement;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -19,18 +20,19 @@ public class MybatisXmlListener implements HotswapHook {
 
     @Override
     public RmiResult executeBefore(HotSwapRmiData rmiData) {
-        byte[] xmlData = rmiData.getFileData();
-        try (ByteArrayInputStream in = new ByteArrayInputStream(xmlData)) {
-            XMLReader xmlReader = XMLReaderFactory.createXMLReader();
-            xmlReader.parse(new InputSource(in));
-            String namespace = (String) xmlReader.getProperty("namespace");
+//        byte[] xmlData = rmiData.getFileData();
+//        try (ByteArrayInputStream in = new ByteArrayInputStream(xmlData)) {
+//            XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+//            xmlReader.parse(new InputSource(in));
+//            String namespace = (String) xmlReader.getProperty("namespace");
+//
+//        } catch (IOException | SAXException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return RmiResult.success();
 
-        } catch (IOException | SAXException e) {
-            throw new RuntimeException(e);
-        }
 
-
-        return RmiResult.success();
+        return null;
     }
 
     @Override
